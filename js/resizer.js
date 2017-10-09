1613c6b2f95364f886eff6f38cea73c6b0b00c6c
thefellowes <marcogeertsma@gmail.com>
2017-10-09 11:59:39 +0200

    First render and scene draw

---
+++ js/resizer.js
@@ -0,0 +1,13 @@
+//Auto resizer when window size changes
+window.addEventListener( 'resize', onWindowResize, false );
+
+/**
+ * Auto resizer when window size changes
+ */
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
