21ae54bcecdf85f07f1dda3d85fceaf7befcadaf
Peter van der Velde <petervandervelde2@gmail.com>
2017-10-10 11:17:16 +0200

    first of many

--- src/js/item.js
+++ src/js/item.js
@@ -1,7 +1,7 @@
 
 /**
  * an basic item class
- * @param {string} name name of aid item 
+ * @param {string} name name of said item 
  * @param {number} value the monetary value of this item
  */
 class Item {
