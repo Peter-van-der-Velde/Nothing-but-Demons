5e8092c8d038910c72262c8f025ca75c8b3a78b0
Pieter <pietergwestra@hotmail.com>
2017-10-16 13:22:56 +0200

    Added mousedown and click detection to input.js

--- src/js/input.js
+++ src/js/input.js
@@ -1,3 +1,4 @@
+"use strict";
 class Input {
   constructor() {
     this.left = false;
@@ -5,9 +6,29 @@ class Input {
     this.up = false;
     this.down = false;
     this.space = false;
+
+    this.mouseDown = false;
+    this.click = false;
   }
 
   update() {
+    this.click = false;
+
+    document.onmousedown = (e) => {
+      e = e || window.event;
+
+      if (e.buttons === 1) {
+        this.mouseDown = true;
+      }
+    }
+
+    document.onmouseup = (e) => {
+      e = e || window.event;
+
+      this.click = true;
+      this.mouseDown = false;
+    }
+
     document.onkeydown = (e) => {
       e = e || window.event;
 
